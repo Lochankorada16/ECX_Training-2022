3be4d9637984e5d4aa6b260f8b4745c041801513
KoradaLochanDurga.S <KoradaLochanDurga.S@EXIND3010-149.allfon.in>
2022-04-07 11:14:48 +0530

    First Commit

---
+++ QuickStart/src/com/training/Application.java
@@ -0,0 +1,20 @@
+package com.training;
+
+public class Application {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Greeting grtObj = new Greeting();
+		
+		// System s=new System(); //
+		// System has a private constructor //
+		
+		System.out.println(grtObj.getMesage());
+		
+		System.out.println(Greeting.getInfo());
+		
+		// System.out.println(grtObj.getInfo()); //
+		
+		
+	}
+}
---
+++ QuickStart/src/com/training/Greeting.java
@@ -0,0 +1,16 @@
+package com.training;
+
+public class Greeting {
+
+
+	public String getMesage() {		
+		return "Welcome to Java Programming";
+	}
+	
+	public static String getInfo() {
+		
+		return "Hello from static method";
+		
+	}
+	
+}
